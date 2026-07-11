18ac93b3567f97d0cc25fc4719c271a73da02d3d
Ankita Dixit <adixit@zacks.com>
2026-07-11 12:50:21 +0530

    edits

--- components/catalog/BackToCatalogButton.tsx
+++ components/catalog/BackToCatalogButton.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export default function BackToCatalogButton({
   className,
-  fallbackHref = "/",
+  fallbackHref="/",
   children,
 }: Props) {
   const router = useRouter();
